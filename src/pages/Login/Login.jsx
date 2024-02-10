32c4bd480506e1e7ffd22b5db97655186cdf67a9
shivam <shivamsangal74@gmail.com>
2024-02-10 16:48:04 +0530

    login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,73 @@
+// src/components/Login.js
+import React from "react";
+import { Form, Input, Button, Card, Typography } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+
+import "./Login.scss";
+
+const { Title } = Typography;
+
+export const Login = () => {
+  const onFinish = (values) => {
+    // Implement your login logic here
+    console.log("Received values:", values);
+  };
+  
+
+  return (
+    <>
+      <div className="background-container" />
+      <div className="login-container">
+        <Card className="card-container">
+          <Title level={3} className="form-title">
+            Login
+          </Title>
+          <Form
+            name="loginForm"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              name="mobileno"
+              rules={[
+                { required: true, message: "Please enter your mobile no!" },
+              ]}
+            >
+              <Input
+                count={{
+                  show: true,
+                  max: 10,
+                }}
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="mobile no"
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please enter your password!" },
+              ]}
+            >
+              <Input.Password
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                placeholder="Password"
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                shape="round"
+                style={{ width: "100%", marginTop: 8 }}
+              >
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
+    </>
+  );
+};
